74048376ddc1deb060ea86eb4fcfc1db34cb7dea
John David Eriksen <johndavid@leadtune.com>
2013-06-22 14:06:23 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bball::Application.config.secret_key_base = '3850667c86e1c98f4ea7f91e3872852eed633f423c9a7fb872797ddd3ce68fdbec726daa9c4a10fa7cf2cd5676b2a8702cb81c35cab1bd5331dd1aae1213422f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bball::Application.config.session_store :cookie_store, key: '_bball_session'
