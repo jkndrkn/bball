4dd9b6e7a115865f37881cd758d211e94abac519
John David Eriksen <johndavid@leadtune.com>
2013-06-23 14:24:05 -0400

    removes superfluous methods

--- app/controllers/player_statistics_controller.rb
+++ app/controllers/player_statistics_controller.rb
@@ -1,7 +1,4 @@
 class PlayerStatisticsController < ApplicationController
-  def index
-  end
-  
   def show
     respond_to do |format|
       format.json do
@@ -10,18 +7,5 @@ class PlayerStatisticsController < ApplicationController
         render :json => data_table.to_json
       end
     end
-  end
-  
-  private
-  
-  def player_stats(player)
-    [player.given_name,
-     player.surname,
-     player.batting_average,
-     player.home_runs,
-     player.steals,
-     player.rbi,
-     player.runs,
-     player.ops]
   end
 end
