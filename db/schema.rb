00dfe161925f99338cb57bb6a8fd2931c9176f8a
John David Eriksen <johndavid@leadtune.com>
2013-06-23 12:53:11 -0400

    switches from mysql to postgresql for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130622192526) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: true do |t|
     t.string   "surname",                     null: false
     t.string   "given_name",                  null: false
